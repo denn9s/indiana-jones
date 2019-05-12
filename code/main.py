2a9351586a950e2799eea490a56beaf6428beeba
Allen Fann <theallenfann@gmail.com>
2019-05-11 23:17:59 -0700

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,15 +1,20 @@
-import MalmoPython
-import logging
-import os
-import json
-import sys
-import math
-import time
-import random
+# ==============================================================================
+# MODULES
+# ==============================================================================
+import MalmoPython					# Malmo
+import logging						# world debug prints
+import time							# sleep for a few ticks every trial
 
-# log to file/terminal
+import world						# world manipulation/observation functions
+from dodger import Dodger			# dodger ai
+
+# ==============================================================================
+# GLOBAL VARS
+# ==============================================================================
+# log to file / terminal
 #logging.basicConfig(filename="log.txt", level=logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG)
+#logging.getLogger().disabled = True
 
 # external file info
 mission_xml = "mission.xml"
@@ -17,77 +22,18 @@ mission_xml = "mission.xml"
 # ai info
 health = 10
 
-def handle_args(agent_host):
-	# ideally run with no arguments
-	try:
-		agent_host.parse(sys.argv)
-	except RuntimeError as e:
-		logging.error("ERROR:", e)
-		logging.info(agent_host.getUsage())
-		exit(1)
-	if agent_host.receivedArgument("help"):
-		print(agent_host.getUsage())
-		exit(0)
-	if agent_host.receivedArgument("test"):
-		exit(0)
-
-def start_mission(agent_host, mission_xml):
-	# get contents of a provided mission xml filepath
-	with open(mission_xml, 'r') as f:
-		mission_file = f.read()
-		mission = MalmoPython.MissionSpec(mission_file, True)
-		logging.info("successfully obtained mission")
-
-	# set up camera POV and recording
-	mission.setViewpoint(0)
-	mission_record = MalmoPython.MissionRecordSpec()
-		
-	# attempt to start a mission:
-	num_tries = 3
-	for attempt in range(num_tries):
-		try:
-			agent_host.startMission(mission, mission_record)
-			logging.info("successfully attempted to started mission")
-			break
-		except RuntimeError as e:
-			if attempt == num_tries - 1:
-				logging.error("could not start mission: ", e)
-				exit(1)
-			else:
-				time.sleep(2)
-	
-	# update world state
-	logging.info("waiting for mission to start")
-	world_state = agent_host.getWorldState()
-	while not world_state.has_mission_begun:
-		time.sleep(0.1)
-		world_state = agent_host.getWorldState()
-	logging.info("successfully updated world state")
-		
-def set_world_observations(agent_host, ep_waiting):
-	world_state = agent_host.getWorldState()
-	if world_state.number_of_observations_since_last_state > 0:
-		msg = world_state.observations[-1].text
-		ob = json.loads(msg)
-		
-		# get current position
-		curr_pos = [0, 0, 0]
-		if "XPos" in ob:
-			curr_pos[0] = int(ob[u'XPos'])
-		if "YPos" in ob:
-			curr_pos[1] = int(ob[u'YPos'])
-		if "ZPos" in ob:
-			curr_pos[2] = int(ob[u'ZPos'])
-		
-		print("curr_pos: ", curr_pos)
-
-
-		
-if __name__ == '__main__':
+# ==============================================================================
+# MAIN
+# ==============================================================================
+def main():
 	# get agent_host
 	agent_host = MalmoPython.AgentHost()
-	handle_args(agent_host)
+	world.handle_args(agent_host)
 	
+	# create ai
+	dodger = Dodger(agent_host)
+	
+	# continuously repeat trials
 	num_reps = 30000
 	for rep in range(num_reps):
 	
@@ -95,12 +41,16 @@ if __name__ == '__main__':
 		if health <= 0 or rep == 0:
 			logging.info("(re)starting mission")
 			agent_host.sendCommand("quit")
-			start_mission(agent_host, mission_xml)
+			world.start_mission(agent_host, mission_xml)
+			time.sleep(0.5)
+			world.refresh(agent_host, dodger)
 
 		# mission in progress
 		else:
-			logging.info("mission in progress...")
-			set_world_observations(agent_host, True)
-			time.sleep(0.5)
+			dodger.run()
+			time.sleep(0.01)
+			
 	time.sleep(1)
-	
+
+if __name__ == '__main__':
+	main()
