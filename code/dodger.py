5af7c9bf1d9f5af309546b9ff4fe5cc207d178c9
Allen Fann <theallenfann@gmail.com>
2019-05-11 23:17:45 -0700

    Add files via upload

--- code/dodger.py
+++ code/dodger.py
@@ -1,11 +1,136 @@
+# ==============================================================================
+# MODULES
+# ==============================================================================
+import MalmoPython					# Malmo
+import logging						# world debug prints
+import time							# sleep for a few ticks every trial
+import random						# random chance to choose actions
+import world						# world observation
+import sys							# max int tau
+from collections import deque		# states/actions/rewards history
 
-class dodger(object):
-	def __init__(self, alpha=0.3, gamma=1, n=1):
-		self.epsilon = 0.2,		# chance of taking a random action
-		self.q_table = {},		# N tiles * M hazard positions * O hazards
-		self.n = n,				# number of back steps to update
-		self.alpha = alpha,		# learning rate
-		self.gamma = gamma		# value decay rate
+# ==============================================================================
+# AI class
+# ==============================================================================
+class Dodger(object):
+	def __init__(self, agent_host, alpha=0.3, gamma=1, n=1):
+		self.alpha = alpha				# learning rate
+		self.gamma = gamma				# value decay rate
+		self.n = n						# number of back steps to update
+		self.epsilon = 0.2				# chance of taking a random action
+		self.q_table = {}
+		self.agent_host = agent_host	
+		self.ep_running = False
+		self.ep_finish = False
+	
+	def update_q_table(self, tau, S, A, R, T):
+		curr_s, curr_a, curr_r = S.popleft(), A.popleft(), R.popleft()
+		G = sum([self.gamma ** i * R[i] for i in range(len(S))])
+		if tau + self.n < T:
+			G += self.gamma ** self.n * self.q_table[S[-1]][A[-1]]
 
-	def run():
+		old_q = self.q_table[curr_s][curr_a]
+		self.q_table[curr_s][curr_a] = old_q + self.alpha * (G - old_q)
+
+
+	def get_curr_feedback(self):
+		#TODO: what defines our reward?
+		return 0
+		
+	def calculate_reward(self):
+		#TODO: what defines our reward?
+		return 0
+
+	def choose_action(self, curr_state, possible_actions):
+		# new state
+		if curr_state not in self.q_table.keys():
+			self.q_table[curr_state] = {"move 1":0, "move 0":0}
+		
+		# chance to choose a random action
+		if random.random() < self.epsilon:
+			rand_action_i = random.randint(0, len(possible_actions) - 1)
+			return possible_actions[rand_action_i]
+		
+		# epsilon greedy policy
+		states = dict(self.q_table[curr_state].items())
+		best_value = max(states.values())
+		best_actions = []
+		for action in possible_actions:
+			if self.q_table[curr_state][action] == best_value:
+				best_actions.append(action)
+		
+		# no best action should never occur... just in case though	
+		if len(best_actions) == 0:
+			rand_action_i = random.randint(0, len(possible_actions) - 1)
+			return possible_actions[rand_action_i]
+			
+		rand_action_i = random.randint(0, len(best_actions) - 1)
+		return best_actions[rand_action_i]
+
+	def get_curr_state(self):
+		# TODO: what defines our state?
+		return tuple([0])
+		
+	def run(self):
+		S, A, R = deque(), deque(), deque()
+		running = True
+		while running:
+			obs = world.get_world_observations(self.agent_host)
+			# TODO: do things with obs... use/make helper functions in world.py
+			
+			if self.ep_running == True:
+				# get current state and the best action from it
+				s0 = self.get_curr_state()
+				possible_actions = ["move 1", "move 0"]
+				a = self.choose_action(s0, possible_actions)
+				
+				# add state/action/reward to history
+				S.append(s0)
+				A.append(a)
+				R.append(0)
+				
+				T = sys.maxsize
+				for t in range(sys.maxsize):
+					obs = world.get_world_observations(self.agent_host)
+					# TODO: same as above todo
+					
+					if t < T:
+						if self.ep_finish == True:
+							# end state and get final reward
+							T = t + 1
+							state.append(None)
+							reward = self.calculate_reward()
+							R.append(reward)
+							self.ep_finish = False
+						else:
+							# act
+							# TODO: how to move a perfect 1 square?
+							self.agent_host.sendCommand(A[-1])
+							time.sleep(0.25)
+							self.agent_host.sendCommand("move 0")
+							
+							# get reward
+							#time.sleep(???) wait a bit and get feedback?
+							reward = self.get_curr_feedback()
+							R.append(reward)
+				
+							# get current state and the best action from it
+							s0 = self.get_curr_state()
+							possible_actions = ["move 1", "move 0"]
+							a = self.choose_action(s0, possible_actions)
+		
+							# add state/action to history
+							S.append(s0)
+							A.append(a)
+						
+						# TODO: wtf is this
+						tau = t - self.n + 1
+						if tau >= 0:
+							self.update_q_table(tau, S, A, R, T)
+						if tau == T - 1:
+							while len(S) > 1:
+								tau = tau + 1
+							self.update_q_table(tau, S, A, R, T)
+							running = False
+							break
 		return
---
+++ code/world.py
@@ -0,0 +1,124 @@
+# ==============================================================================
+# MODULES
+# ==============================================================================
+import MalmoPython					# Malmo
+import logging						# world debug prints
+import time							# sleep for a few ticks every trial
+import sys							# sys arguments
+import json							# read world observations
+
+# ==============================================================================
+# AGENT/MISSION FUNCTIONS
+# ==============================================================================
+def handle_args(agent_host):
+	# ideally run with no arguments
+	try:
+		agent_host.parse(sys.argv)
+	except RuntimeError as e:
+		logging.error("ERROR:", e)
+		logging.info(agent_host.getUsage())
+		exit(1)
+	if agent_host.receivedArgument("help"):
+		print(agent_host.getUsage())
+		exit(0)
+	if agent_host.receivedArgument("test"):
+		exit(0)
+
+def start_mission(agent_host, mission_xml):
+	# get contents of a provided mission xml filepath
+	logging.info("opening mission xml")
+	with open(mission_xml, 'r') as f:
+		mission_file = f.read()
+		mission = MalmoPython.MissionSpec(mission_file, True)
+		logging.info("successfully opened mission xml")
+
+	# set up camera POV and recording
+	mission.setViewpoint(0)
+	mission_record = MalmoPython.MissionRecordSpec()
+		
+	# attempt to start a mission:
+	num_tries = 3
+	logging.info("attempting to start mission")
+	for attempt in range(num_tries):
+		try:
+			agent_host.startMission(mission, mission_record)
+			logging.info("successfully attempted to started mission")
+			break
+		except RuntimeError as e:
+			if attempt == num_tries - 1:
+				logging.error("could not start mission")
+				exit(1)
+			else:
+				logging.info("retrying...")
+				time.sleep(2)
+	
+	# update world state
+	logging.info("waiting for mission to start")
+	world_state = agent_host.getWorldState()
+	while not world_state.has_mission_begun:
+		time.sleep(0.1)
+		world_state = agent_host.getWorldState()
+	logging.info("successfully updated world state")
+
+# ==============================================================================
+# WORLD MANIPULATION/OBSERVATION FUNCTIONS
+# ==============================================================================
+def get_dispensers(agent_host):
+	# get the x-y-z coordinates of all dispensers in view
+	world_state = agent_host.getWorldState()
+	if world_state.number_of_observations_since_last_state > 0:
+		msg = world_state.observations[-1].text
+		obs = json.loads(msg)
+		view_left = obs.get(u'view_left', 0)
+		dispensers = []
+		for i in range(len(view_left)):
+			if view_left[i] == "dispenser":
+				coord = (obs[u'XPos']+4.5, obs[u'YPos']+1, obs[u'ZPos']+i-0.5)
+				dispensers.append(coord)
+		return dispensers
+	else:
+		logging.error("cannot view dispensers")
+		exit(1)
+
+def refill_arrows(agent_host, dispensers):
+	c1 = "/replaceitem block "
+	c2 = "slot.container."
+	c3 = " minecraft:arrow 64"
+	for x, y, z in dispensers:
+		xyz = str(int(x)) + " " + str(int(y)) + " " + str(int(z))
+		for slot in range(4):
+			agent_host.sendCommand(c1 + xyz + c2 + str(slot) + c3)
+
+def refresh(agent_host, dodger):
+	# refill dispensers
+	dispensers = get_dispensers(agent_host)
+	refill_arrows(agent_host, dispensers)
+
+	# set new episode
+	dodger.ep_running = True
+	dodger.ep_finish = False
+	
+def get_curr_pos(obs):
+	# get a dict of curent x-y-z positions
+	return {"x":obs[u'XPos'], "y":obs[u'YPos'], "z":obs[u'ZPos']}
+
+def get_arrow_pos(obs):
+	# get a dict of arrow positions by their z-positions
+	arrows = {}
+	arrow_l, arrow_r = curr_pos["x"]+4.2, curr_pos["x"]-2.4
+	for entity in obs["entities"]:
+		if entity["name"] == "Arrow" and entity["motionX"] < -0.01 and \
+		   entity["x"] < arrow_l and entity["x"] > arrow_r:
+				arrows[int(entity["z"])] = entity
+
+def get_world_observations(agent_host):
+	world_state = agent_host.getWorldState()
+	if world_state.number_of_observations_since_last_state > 0:
+		msg = world_state.observations[-1].text
+		obs = json.loads(msg)  
+
+		# validate entities exist
+		if not "entities" in obs:
+			logging.error("no entities in the world")
+			exit(1)
+		return obs
